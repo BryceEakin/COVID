98f7026fcba61ac10fdd7425f10cc9a5dab37dd6
Bryce Eakin <bryce.eakin@outlook.com>
2020-04-15 01:23:38 -0400

    Improved checkpointing, minor refactor

--- covid/datasets.py
+++ covid/datasets.py
@@ -7,11 +7,15 @@ import pandas as pd
 import pickle as pkl
 import gzip
 
+import random
+
 from .utils import CHEMPROP_ARGS
 
 __ALL__ = [
     'collate_stitch_data', 
-    'StitchDataset'
+    'StitchDataset',
+    'create_dataloader',
+    'create_data_split'
 ]
 
 def collate_stitch_data(list_of_samples):
@@ -83,3 +87,93 @@ class StitchDataset(T.utils.data.Dataset):
         targets = T.tensor(row[['activation', 'binding', 'catalysis', 'inhibition', 'reaction']].values.astype(float), dtype=T.float32)
         
         return chem_graph, chem_features, protein, targets
+
+
+def create_dataloader(data, batch_size, sample_size=None, **dl_kwargs):
+    class SubSampler(T.utils.data.Sampler):
+        def __init__(self, length, numsamples):
+            self._length = length
+            self._numsamples = min(numsamples, length)
+
+        def __iter__(self):
+            return iter(np.random.choice(self._length, self._numsamples, replace=False))
+
+        def __len__(self):
+            return self._numsamples
+
+    return T.utils.data.DataLoader(
+        data,
+        batch_size=batch_size,
+        sampler=None if sample_size is None else SubSampler(len(data), sample_size),
+        shuffle=True if sample_size is None else None,
+        collate_fn = collate_stitch_data,
+        **dl_kwargs
+    )
+
+
+def create_data_split(src_folder, dst_train_folder, dst_test_folder, pct_train=0.9, tolerance = 0.01):
+    data = StitchDataset(src_folder)
+    data._deferred_load()
+    
+    while True:
+        is_selected = pd.Series(False, index=data.all_data.index)
+
+        chem_to_select = list(data.all_chemicals.keys())
+        random.shuffle(chem_to_select)
+
+        have_selected = set()
+        num_selected = 0
+
+        while num_selected/is_selected.shape[0] < (1-pct_train) and len(chem_to_select) > 0:
+            chem = chem_to_select.pop(0)
+            items_to_select = [chem]
+            while items_to_select:
+                item = items_to_select.pop(0)
+                # print(len(have_selected), len(items_to_select), is_selected.mean())
+
+                have_selected.add(item)
+                new_selections = (
+                    (data.all_data['item_id_a'] == item)
+                    #| (data.all_data['item_id_b'] == item)
+                )
+                new_data = data.all_data.loc[new_selections]
+                items_to_select.extend(
+                    set(new_data['item_id_a'].values).union(
+                        set() #new_data['item_id_b'].values
+                    ).difference(have_selected)
+                )
+                is_selected = is_selected | new_selections
+                num_selected += new_data.shape[0]
+
+        if is_selected.mean() <= (1-pct_train) + tolerance:
+            break
+        
+        print(f"Test set outside of tolerance -- {is_selected.mean():0.3%} -- retrying...")
+    
+    print(f"Test set selected -- {is_selected.mean():0.3%}")
+    
+    train_data = data.all_data.loc[~is_selected]
+    train_chem = {k:data.all_chemicals[k] for k in train_data['item_id_a'].unique()}
+    train_prot = {k:data.all_proteins[k] for k in train_data['item_id_b'].unique()}
+    
+    test_data = data.all_data.loc[is_selected]
+    test_chem = {k:data.all_chemicals[k] for k in test_data['item_id_a'].unique()}
+    test_prot = {k:data.all_proteins[k] for k in test_data['item_id_b'].unique()}
+    
+    if not os.path.exists(dst_train_folder):
+        os.mkdir(dst_train_folder)
+        
+    train_data.to_csv(os.path.join(dst_train_folder, 'stitch_preprocessed.csv.gz'), index=False)
+    with gzip.open(os.path.join(dst_train_folder, 'stitch_proteins.pkl.gz'), 'wb') as f:
+        pkl.dump(train_prot, f, pkl.HIGHEST_PROTOCOL)
+    with gzip.open(os.path.join(dst_train_folder, 'stitch_chemicals.pkl.gz'), 'wb') as f:
+        pkl.dump(train_chem, f, pkl.HIGHEST_PROTOCOL)
+        
+    if not os.path.exists(dst_test_folder):
+        os.mkdir(dst_test_folder)
+        
+    test_data.to_csv(os.path.join(dst_test_folder, 'stitch_preprocessed.csv.gz'), index=False)
+    with gzip.open(os.path.join(dst_test_folder, 'stitch_proteins.pkl.gz'), 'wb') as f:
+        pkl.dump(test_prot, f, pkl.HIGHEST_PROTOCOL)
+    with gzip.open(os.path.join(dst_test_folder, 'stitch_chemicals.pkl.gz'), 'wb') as f:
+        pkl.dump(test_chem, f, pkl.HIGHEST_PROTOCOL)
--- covid/model.py
+++ covid/model.py
@@ -1,26 +1,42 @@
 import torch as T
 from torch import nn
+import torch.nn.functional as F
 
 from .data import apply_to_protein_batch, ProteinBatchToPaddedBatch
 from .modules import create_resnet_block_1d, DownscaleConv1d, Squeeze
 
+import numpy as np
+
+__all__ = [
+    "CovidModel",
+    "RandomModel",
+    'create_protein_model',
+    'run_model',
+    'calculate_average_loss_and_accuracy'
+]
 
 class CovidModel(nn.Module):
-    def __init__(self, chem_model, protein_model, in_dim=900, dropout=0.0, out_dim=5):
+    def __init__(self, chem_model, protein_model, layers=2, dropout=0.0, in_dim=900, hidden_dim=512, out_dim=5):
         super().__init__()
         
         self.chem_model = chem_model
         self.protein_model = protein_model
         
-        self.final_layers = nn.Sequential(
-            nn.Linear(in_dim, 512),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(512,256),
-            nn.ReLU(),
-            nn.Linear(256,out_dim),
-            nn.Sigmoid()
-        )
+        self.final_layers = nn.Sequential(*(
+            [
+                nn.Linear(in_dim, hidden_dim),
+                nn.ReLU(),
+                nn.Dropout(dropout),
+            ] + sum([[
+                nn.Linear(hidden_dim,hidden_dim),
+                nn.ReLU(),
+            ] for _ in range(layers-1)],[])
+            + [
+                nn.Dropout(dropout),
+                nn.Linear(hidden_dim,out_dim),
+                nn.Sigmoid()
+            ]
+        ))
         
     def forward(self, chem_batch, chem_f_batch, protein_batch):
         chem_out = self.chem_model(chem_batch, chem_f_batch)
@@ -30,14 +46,53 @@ class CovidModel(nn.Module):
         return result
 
 
+class RandomModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(5,100),
+            nn.ReLU(),
+            nn.Linear(100,5),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, chem_graphs, chem_features, proteins):
+        batch_size = chem_graphs.n_mols
+        input = T.rand((batch_size, 5), device=chem_features.device)
+        return self.model(input)
+
+
+def run_model(model, batch, device):
+    chem_graphs, chem_features, proteins, target = batch
+    
+    chem_graphs = chem_graphs.to(device)
+    chem_features = chem_features.to(device)
+    proteins = proteins.to(device)
+
+    weights = (target * 0.0015).clamp(1e-3, 1.0)
+    weights[target == 0] = 0.5
+
+    weights = weights.to(device)
+    
+    target = (1.0*(target > 0)).to(device)
+    
+    result = model(chem_graphs, chem_features, proteins)
+
+    loss = F.binary_cross_entropy(result, target, weight=weights)
+    return result, target, loss, weights
+
+
 def create_protein_model(dropout = 0.2, outdim=600):
     return nn.Sequential(
         # 21->100 channels inplace convolution
         apply_to_protein_batch(nn.Conv1d(23, 512, (1, ), 1, 0)),
+        apply_to_protein_batch(nn.Dropout(dropout)),
         
         #Do some resnet
         create_resnet_block_1d(512, 64, inner_kernel=3, for_protein_batch=True),
         create_resnet_block_1d(512, 64, inner_kernel=5, for_protein_batch=True),
+        apply_to_protein_batch(nn.Dropout(dropout)),
+
         create_resnet_block_1d(512, 64, inner_kernel=7, for_protein_batch=True),
         create_resnet_block_1d(512, 64, inner_kernel=11, for_protein_batch=True),
         
@@ -69,3 +124,34 @@ def create_protein_model(dropout = 0.2, outdim=600):
         #nn.Tanhshrink(),
         #nn.Tanh(),
     )
+
+def calculate_average_loss_and_accuracy(model, dl, device):
+    model.eval()
+    
+    total_loss = 0.0
+    total_div = 0
+    
+    accuracy_acc = np.zeros(5)
+    accuracy_div = np.zeros(5)
+    
+    tp = np.zeros(5)
+    fp = np.zeros(5)
+    fn = np.zeros(5)
+    tn = np.zeros(5)
+    
+    for batch in dl:
+        chem_graphs, chem_features, proteins, target = batch
+        result, target, loss, weight = run_model(model, batch, device)
+        
+        total_loss += loss.item() * result.shape[0]
+        total_div += result.shape[0]
+        
+        accuracy_acc += (((result > 0.5) == target) * weight).sum(0).cpu().numpy()
+        accuracy_div += weight.sum(0).cpu().numpy()
+        
+        tp += (((result >= 0.5) * target) * weight).sum(0).cpu().numpy()
+        fp += (((result >= 0.5) * (1 - target)) * weight).sum(0).cpu().numpy()
+        fn += (((result < 0.5) * target) * weight).sum(0).cpu().numpy()
+        tn += (((result < 0.5) * (1 - target)) * weight).sum(0).cpu().numpy()
+        
+    return total_loss / total_div, accuracy_acc / accuracy_div, (tp,fp,fn,tn)
