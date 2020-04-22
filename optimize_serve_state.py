1dc23ecebf32ffc53ba5d550c5d5497f23238814
Bryce Eakin <bryce.eakin@outlook.com>
2020-04-21 21:27:01 -0400

    Updated webserver

--- optimize_serve_state.py
+++ optimize_serve_state.py
@@ -1,5 +1,5 @@
 from sanic import Sanic
-from sanic.response import html, file_stream, text
+from sanic.response import html, file_stream, text, redirect
 from sanic.exceptions import NotFound
 
 import sys, os
@@ -20,6 +20,8 @@ import hyperopt
 from hyperopt import hp
 
 from collections import Counter, defaultdict
+
+PORT = 5000
 
 SEARCH_SPACE = {
     'synthetic_negative_rate': hp.uniform('neg_rate', 0,1),
@@ -56,8 +58,6 @@ def fig_to_base64(fig, close=False, **save_kwargs):
         plt.close(fig)
     return result
 
-PORT = 5535
-
 app = Sanic(name='CovidProject')
 TRIALS = MongoTrials('mongo://localhost:1234/covid/jobs')
 
@@ -86,12 +86,76 @@ async def put_training_state(request, run_id):
     shutil.move(f"./training_state/{run_id}__state.pkl.gz.tmp", f"./training_state/{run_id}__state.pkl.gz")
     return text("Model State Uploaded")
 
-def make_button(to, icon, disabled=False):
+def make_button(to, icon, disabled=False, text=None):
     return f"""
         <a class="btn btn-info px-3 {'disabled' if disabled else ''}" role="button" href="{to}">
-            <i class="fa fa-{icon}"></i>
+            <i class="fa fa-{icon}"></i>{' ' + text if text is not None else ''}
         </a>
     """
+
+@app.get("/status")
+async def get_status(request):
+    if 'refresh' in request.args:
+        TRIALS.refresh()
+
+    state_lookup = {getattr(hyperopt, k):k for k in ['JOB_STATE_NEW', 'JOB_STATE_ERROR', 'JOB_STATE_RUNNING', 'JOB_STATE_DONE']}
+    state_lookup[-1] = 'JOB_STATE_Prev-Level Hints'
+    counters = defaultdict(lambda: Counter())
+
+    for t in TRIALS.trials:
+        if 'training_loss_hist' in t['result'] and t['state'] == hyperopt.JOB_STATE_DONE:
+            epoch = int(t['result']['training_loss_hist'][-1][0])
+            if t['exp_key'] != f'covid-{epoch}':
+                counters[t['exp_key']][-1] += 1
+            else:
+                counters[t['exp_key']][t['state']] += 1
+        else:
+            counters[t['exp_key']][t['state']] += 1
+
+    status_table = f"""
+        <table class="table table-striped table-sm w-auto mt-4 ml-1">
+        <thead class="thead-light">
+        <th>Hyperopt Pass</th><th>{'</th><th>'.join(state_lookup[i][10:].title() for i in range(-1,4))}</th>
+        </thead>
+        <tbody>
+        {''.join('<tr><th>' + k + '</th><td>' + '</td><td>'.join(
+            str(counters[k][c]) for c in range(-1,4)
+        ) + '</td></tr>' for k in counters.keys())}
+        </tbody>
+        <table>
+    """
+
+    running = [t for t in TRIALS.trials if t['state'] in (
+        hyperopt.JOB_STATE_RUNNING,
+        hyperopt.JOB_STATE_NEW
+    ) and t['owner'] is not None]
+    queued = [t for t in TRIALS.trials if t['state'] == hyperopt.JOB_STATE_NEW and t not in running]
+
+    return html(f"""
+        <html>
+        <head><meta name="viewport" content="width=device-width, initial-scale=1"/></head>
+        <body>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <div class="col-md-12 mb-4 mt-4 text-center">
+        {make_button(f"/best-trials/0", "eye", text="Review Models")}
+        <br><br>
+        {make_button(f"/status?refresh=True", "refresh")}
+        </div>
+        <h2>Optimizer Generations</h2>
+        {status_table}
+        <h2>Active Jobs</h2>
+        <table class="table table-striped table-sm w-auto ml-1">
+        <thead class="thead-light">
+        <th>Node</th><th>Generation</th><th>Id</th>
+        </thead><tbody>
+        <tr>{'</tr><tr>'.join(f"<td>{t['owner'][0] if t['owner'] is not None else 'n/a'}</td><td>{t['exp_key']}</td><td>{t['tid']}</td>" for t in running)}</tr>
+        </tbody></table>
+        <h2>{len(queued)} items queued</h2>
+        </body>
+        </html>
+    """)
+
 
 @app.get("/best-trials/<n:int>")
 async def get_current_best(request, n=0):
@@ -116,45 +180,18 @@ async def get_current_best(request, n=0):
             losses.append(loss)
 
     idx_list = np.argsort([x if x is not None else np.inf for x in losses])
-    
+    max_idx = np.argwhere(np.isfinite(np.array(losses)[idx_list])).flatten().max()
+
+    if n > max_idx:
+        return redirect(f"/best-trials/{max_idx}")
+
     tr = trials[idx_list[n]]
     fig = get_performance_plots(tr['result']['training_loss_hist'], tr['result']['validation_stats'])
     img = fig_to_base64(fig, close=True).decode('utf-8')
-    first_btn = make_button(f"/best-trials/0", "fast-backward")
-    prev_btn = make_button(f"/best-trials/{n-1}", "chevron-left", n == 0)
-    refresh_btn = make_button(f"/best-trials/{n}?refresh=True", "repeat")
-    next_btn = make_button(f"/best-trials/{n+1}", "chevron-right")
     
     stats = get_performance_stats(tr['result']['validation_stats'])
 
     params = hyperopt.space_eval(SEARCH_SPACE, {k:v[0] for k,v in tr['misc'].get('vals',{}).items()})
-
-    state_lookup = {getattr(hyperopt, k):k for k in ['JOB_STATE_NEW', 'JOB_STATE_ERROR', 'JOB_STATE_RUNNING', 'JOB_STATE_DONE']}
-    state_lookup[-1] = 'JOB_STATE_Prev-Level Hints'
-    counters = defaultdict(lambda: Counter())
-
-    for t in TRIALS.trials:
-        if 'training_loss_hist' in t['result'] and t['state'] == hyperopt.JOB_STATE_DONE:
-            epoch = int(t['result']['training_loss_hist'][-1][0])
-            if t['exp_key'] != f'covid-{epoch}':
-                counters[t['exp_key']][-1] += 1
-            else:
-                counters[t['exp_key']][t['state']] += 1
-        else:
-            counters[t['exp_key']][t['state']] += 1
-
-    status_table = f"""
-        <table class="table table-striped table-sm w-auto mt-4">
-        <thead class="thead-light">
-        <th>Hyperopt Pass</th><th>{'</th><th>'.join(state_lookup[i][10:].title() for i in range(-1,4))}</th>
-        </thead>
-        <tbody>
-        {''.join('<tr><th>' + k + '</th><td>' + '</td><td>'.join(
-            str(counters[k][c]) for c in range(-1,4)
-        ) + '</td></tr>' for k in counters.keys())}
-        </tbody>
-        <table>
-    """
 
     return html(f"""
         <html>
@@ -163,13 +200,17 @@ async def get_current_best(request, n=0):
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <div>
-        <h3>Status</h3>
-        {status_table}
-        </div>
         <h3>Run Details</h3>
         <div class="col-md-12 mb-4 mt-4 text-center">
-        {first_btn}{prev_btn}{refresh_btn}{next_btn}<br><br>
+        {make_button(f"/status", "server", text="Job Status")}
+        <br><br>
+        {make_button(f"/best-trials/0", "fast-backward", n==0)}
+        {make_button(f"/best-trials/{n-1}", "chevron-left", n == 0)}
+        {make_button(f"/best-trials/{n}?refresh=True", "refresh")}
+        {make_button(f"/best-trials/{n+1}", "chevron-right", n == max_idx)}
+        {make_button(f"/best-trials/{max_idx}", "fast-forward", n == max_idx)}
         </div>
+        <br>
         <table class="table table-striped table-sm w-auto ml-1">
         <tbody>
         <tr><th>Pass</th><td>{tr["exp_key"]}</td></tr>
